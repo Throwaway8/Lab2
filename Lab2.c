2c8b83860e4ae1d4e851b57782fce04656e7b266
Thom <44063708+Throwaway8@users.noreply.github.com>
2019-03-07 12:27:35 -0500

    Update Lab2.c

--- Lab2.c
+++ Lab2.c
@@ -16,6 +16,7 @@ void start()
 	int count3 = 0;
 	int count4 = 0;
 	int count5 = 0;
+	int count6 = 0;
 	int lookUpTable[10];
 	lookUpTable[0] = 0x3F;
 	lookUpTable[1] = 0x6;
@@ -33,40 +34,54 @@ void start()
 	*(hex_ptr + 3) = 0x3F;
 	*(hex_ptr2) = 0x3F;
 	*(hex_ptr2 + 1) = 0x3F;
-	while (1)
+	while (*(Key_ptr) == 0)
 	{
 		*(ctrl_ptr) &= 0x000;
 		*(load_ptr) |= 0xF4240;
 		*(ctrl_ptr) |= 0b011;
-
+		
+		*(hex_ptr) = *lookUpTable + 1;
+		*(hex_ptr+1) = *lookUpTable + 1;
+		*(hex_ptr + 2) = *lookUpTable + 1;
+		*(hex_ptr+3) = *lookUpTable + 1;
+		*(hex_ptr2) = *lookUpTable;
+		*(hex_ptr2+1) = *lookUpTable + 1;
+		
+		
+		
 		if ((*(interrupt_ptr) &= 0b1) == 1)
 		{
-			*(hex_ptr) = *lookUpTable + 1;
+			
 			count++;
-			if (count == 9)
+			if (count == 10)
 			{
-				*(hex_ptr+1) = *lookUpTable + 1;
+				
 				count = 0;
 				count2++;
-				if (count2 == 9)
+				if (count2 == 10)
 				{
-					*(hex_ptr + 2) = *lookUpTable + 1;
+			
 					count2 = 0;
 					count3++;
-					if (count3 == 0)
+					if (count3 == 10)
 					{
-						*(hex_ptr+3) = *lookUpTable + 1;
+						
 						count3 = 0;
 						count4++;
-						if (count4 == 9)
+						if (count4 == 10)
 						{
-							*(hex_ptr2) = *lookUpTable + 1;
+							
 							count4 = 0;
 							count5++;
-							if (count5 == 9)
+							if (count5 == 10)
 							{
-								*(hex_ptr2+1) = *lookUpTable + 1;
+								
 								count5 = 0;
+								count6++;
+								if (count6 == 10)
+								{
+									count6 = 0;
+								}
 							}
 						}
 					}
@@ -78,10 +93,23 @@ void start()
 
 void reset()
 {
-	*(ctrl_ptr) |= 0b100;
-	*(interrupt_ptr) |= 0b1;
-	*(load_ptr) &= 0x0;
-	*(ctrl_ptr) |= 0x1;
+	int lookUpTable[10];
+	lookUpTable[0] = 0x3F;
+	lookUpTable[1] = 0x6;
+	lookUpTable[2] = 0x5B;
+	lookUpTable[3] = 0x4F;
+	lookUpTable[4] = 0x66;
+	lookUpTable[5] = 0x6D;
+	lookUpTable[6] = 0x7D;
+	lookUpTable[7] = 0x7;
+	lookUpTable[8] = 0x7F;
+	lookUpTable[9] = 0x6F;
+	*(hex_ptr) = 0x3F;
+	*(hex_ptr + 1) = 0x3F;
+	*(hex_ptr + 2) = 0x3F;
+	*(hex_ptr + 3) = 0x3F;
+	*(hex_ptr2) = 0x3F;
+	*(hex_ptr2 + 1) = 0x3F;
 }
 
 void stop()
@@ -95,24 +123,24 @@ int main(void) {
 
 	while (1)
 	{
-		if ((*(Key_ptr) &= 0b1) == 1 && (*(Key_ptr) &= 0b10) == 0 && (*(Key_ptr) &= 0b100) == 0)
+		if ((*(Key_ptr) &= 1) == 1) 
 		{
 			start();
 			*(Key_ptr) &= 0x0;
 		}
-		else if ((*(Key_ptr) &= 0b1) == 0 && (*(Key_ptr) &= 0b10) == 1 && (*(Key_ptr) &= 0b100) == 0)
+		else if ((*(Key_ptr) &= 2) == 1)
 		{
 			stop();
 			*(Key_ptr) &= 0x0;
 		}
-		else if ((*(Key_ptr) &= 0b1) == 0 && (*(Key_ptr) &= 0b10) == 0 && (*(Key_ptr) &= 0b100) == 1)
+		else if ((*(Key_ptr) &= 4) == 1)
 		{
 			reset();
 			*(Key_ptr) &= 0x0;
 		}
 		else
 		{
-			*(Key_ptr) &= 0x0;
+			
 		}
 	}
 }
