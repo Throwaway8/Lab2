82cd1c5ba86c5d8adbf010662809cd8ac20f2790
Thom <44063708+Throwaway8@users.noreply.github.com>
2019-04-01 08:47:31 -0700

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,20 @@
 #include <stdlib.h>
 
 volatile int * Hex_ptr = (int*)HEX3_HEX0_BASE;
+volatile int * GPIO = (int*)HPS_GPIO0_BASE;
 
-
-void createWaterData()
+int createWaterData()
 {
-	char waterData[255];
-	for (int i = 0; i <= 255; i++)
+	int a;
+	time_t t;
+	while (1)
 	{
-		waterData[i] = i;
+		srand((unsigned)time(&t));
+		for (int i = 0; i; i++)
+		{
+			a = rand() % 255;
+			return a;
+		}
 	}
 }
 
@@ -19,5 +25,23 @@ void createWaterData()
 
 void main()
 {
+	*(GPIO) |= 1UL << 14;
+
+
+
+	while (1)
+	{
+		int val = createWaterData();
+		
+		if (val <= 50)
+		{
+			*(Hex_ptr) = 0x383F;
+		}
+		else
+		{
+			*(Hex_ptr) = 0x7D3F;
+		}
+
+	}
 
 }
