75efe8d4ed74702a1b9731ddc2af1ed3a1a92b8c
Thom <44063708+Throwaway8@users.noreply.github.com>
2019-04-04 11:39:24 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,3 @@
-#include "address_map_arm.h"
-#include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
 
 volatile int *buttons = (int*)KEY_BASE;
@@ -8,8 +5,10 @@ volatile int *timer_ptr = (int *)HPS_TIMER0_BASE;
 int ms1, ms2, s1, s2, m1, m2 = 0;
 int running = 0;
 volatile int DELAY_LENGTH = 700000;
-volatile int * Hex_ptr = (int*)HEX3_HEX0_BASE;
-volatile int * Hex_ptr2 = (int*)HEX5_HEX4_BASE ;
+volatile unsigned char * Hex_ptr = (unsigned char*)HEX3_HEX0_BASE;
+volatile int * Hex_ptr2 = (int*)HEX3_HEX0_BASE+1;
+volatile int * Hex_ptr3 = (int*)HEX3_HEX0_BASE+2;
+volatile int * Hex_ptr4 = (int*)HEX5_HEX4_BASE ;
 volatile int* led = (int*)LED_BASE;
 
 void config_HPS_timer()
@@ -30,19 +29,29 @@ int RandSensor(){
 }
 
 void ReadHumidity(){
-	
+	int lookUpTable[10];
+    lookUpTable[0] = 0x3F;
+    lookUpTable[1] = 0x6;
+    lookUpTable[2] = 0x5B;
+    lookUpTable[3] = 0x4F;
+    lookUpTable[4] = 0x66;
+    lookUpTable[5] = 0x6D;
+    lookUpTable[6] = 0x7D;
+    lookUpTable[7] = 0x7;
+    lookUpTable[8] = 0x7F;
+    lookUpTable[9] = 0x6F;
+	int i = 0;
 	int RH = RandSensor();
-	
-			
-		if(RH < 60){
-			*(Hex_ptr2) = 0x383F;
-		}
-		else{
-			*(Hex_ptr2) = 0x7D3F;
-		}
-            
 		
-	}
+		int dig0 = RH % 10;
+		int dig1 = (RH/10) % 10;
+		int dig2 = (RH/100) % 10;
+		
+		*(Hex_ptr) = lookUpTable[dig0];
+		*(Hex_ptr+1) = lookUpTable[dig1];
+		*(Hex_ptr+2) = lookUpTable[dig2];
+}
+
 
 void LEDtemp()
 {
@@ -84,11 +93,11 @@ void readWater()
 	int val = RandSensor();
 	if (val <= 10)
 		{
-			*(Hex_ptr) = 0x383F;
+			*(Hex_ptr4) = 0x383F;
 		}
 		else
 		{
-			*(Hex_ptr) = 0x7D3F;
+			*(Hex_ptr4) = 0x7D3F;
 		}
 	
 }
@@ -111,7 +120,7 @@ int delay_count;
 			{
 				if((*(timer_ptr) &= 0b1) == 1){
 					count++;
-					if(count == 100){
+					if(count == 500){
 
 						count = 0;
 					}
@@ -123,4 +132,3 @@ int delay_count;
 		}			
 	}
 }
-
